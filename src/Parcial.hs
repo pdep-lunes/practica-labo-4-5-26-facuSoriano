8d64efd096d9b67e5a4b2fdf7b267058c77c21e1
Facundo <fsoriano@frba.utn.edu.ar>
2026-05-10 12:18:07 -0300

    ejercicio completado

--- src/Parcial.hs
+++ src/Parcial.hs
@@ -3,7 +3,7 @@ data Perro = UnPerro {
     juguetesFav :: [String],
     tiempo :: Int,
     energia :: Int
-}
+} deriving Show
 
 cambiarEnergia :: (Int -> Int) -> Perro -> Perro
 cambiarEnergia unaFuncion unPerro = unPerro{energia = max 0 . unaFuncion . energia $ unPerro }
@@ -12,7 +12,7 @@ jugar :: Perro -> Perro
 jugar  unPerro = cambiarEnergia (subtract 10) unPerro
 
 ladrar :: Perro -> Int -> Perro
-ladrar unPerro ladridos = cambiarEnergia (+ div ladridos 2) unPerro
+ladrar unPerro ladridos = cambiarEnergia (+ (div ladridos 2)) unPerro
 
 regalar :: String -> Perro ->  Perro
 regalar unJuguete unPerro = unPerro {juguetesFav = unJuguete : juguetesFav unPerro}
@@ -34,3 +34,27 @@ diaDeSpa unPerro
     | fiftyMins unPerro || esExtravagante unPerro = cambiarEnergia (const 100) . regalar "peine de goma" $ unPerro
     | otherwise = unPerro
 
+zara :: Perro
+zara = UnPerro "dalmata" ["pelota", "mantita"] 90 80
+
+type Actividad = Perro -> Perro
+
+type Rutina = [(Actividad, Int)]
+
+guarderiaPdePerritos :: Rutina
+guarderiaPdePerritos =
+    [ (jugar, 30)
+    , (ladrar 18, 20)
+    , (regalar "pelota", 0)
+    , (diaDeSpa, 120)
+    , (diaDeCampo, 720)
+    ]
+
+duracionRutina :: Rutina -> Int
+duracionRutina = sum . map snd
+
+puedeEstarEnGuarderia :: Perro -> Rutina -> Bool
+puedeEstarEnGuarderia unPerro rutina = tiempo unPerro > duracionRutina rutina
+
+esResponsable :: Perro -> Bool
+esResponsable unPerro = length(juguetesFav (diaDeCampo unPerro)) >3
